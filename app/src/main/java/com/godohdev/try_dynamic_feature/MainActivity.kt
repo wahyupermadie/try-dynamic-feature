d7d63213d67810b70df5628659d449595b344fa1
Wahyu Permadi <wahyu.permadi1725@gmail.com>
2020-01-13 20:08:00 +0700

    refactor(main-activity): add download dynamic module

--- app/src/main/java/com/godohdev/try_dynamic_feature/MainActivity.kt
+++ app/src/main/java/com/godohdev/try_dynamic_feature/MainActivity.kt
@@ -1,12 +1,110 @@
 package com.godohdev.try_dynamic_feature
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.google.android.play.core.splitinstall.SplitInstallManager
+import com.google.android.play.core.splitinstall.SplitInstallManagerFactory
+import com.google.android.play.core.splitinstall.SplitInstallRequest
+import com.google.android.play.core.splitinstall.SplitInstallStateUpdatedListener
+import com.google.android.play.core.splitinstall.model.SplitInstallSessionStatus
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var splitInstallManager: SplitInstallManager
+    lateinit var request: SplitInstallRequest
+    val DYNAMIC_FEATURE = "tvshow"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initDynamicModules()
+        setClickListeners()
+    }
+
+    private fun initDynamicModules() {
+        splitInstallManager = SplitInstallManagerFactory.create(this)
+        request = SplitInstallRequest
+            .newBuilder()
+            .addModule(DYNAMIC_FEATURE)
+            .build()
+    }
+
+    private fun setClickListeners() {
+        buttonClick.setOnClickListener {
+            if (!isDynamicFeatureDownloaded(DYNAMIC_FEATURE)) {
+                downloadFeature()
+            } else {
+                buttonDeleteNewsModule.visibility = View.VISIBLE
+                buttonOpenNewsModule.visibility = View.VISIBLE
+            }
+        }
+
+        buttonOpenNewsModule.setOnClickListener {
+            val intent = Intent()
+                .setClassName(this, "com.godohdev.news_feature.newsloader.NewsLoaderActivity")
+            startActivity(intent)
+        }
+        buttonDeleteNewsModule.setOnClickListener {
+            val list = ArrayList<String>()
+            list.add(DYNAMIC_FEATURE)
+            uninstallDynamicFeature(list)
+        }
+    }
+
+    private fun uninstallDynamicFeature(list: List<String>) {
+        splitInstallManager.deferredUninstall(list)
+            .addOnSuccessListener {
+                buttonDeleteNewsModule.visibility = View.GONE
+                buttonOpenNewsModule.visibility = View.GONE
+            }
+    }
+
+    private fun isDynamicFeatureDownloaded(feature: String): Boolean {
+        return splitInstallManager.installedModules.contains(feature)
+    }
+
+    @SuppressLint("SwitchIntDef")
+    private fun downloadFeature() {
+        splitInstallManager.startInstall(request)
+            .addOnFailureListener {
+                Log.d("DATA_GUE"," "+it.localizedMessage)
+            }
+            .addOnSuccessListener {
+                Log.d("DATA_GUE"," "+it)
+                buttonOpenNewsModule.visibility = View.VISIBLE
+                buttonDeleteNewsModule.visibility = View.VISIBLE
+            }
+            .addOnCompleteListener {
+                Log.d("DATA_GUE", it.result.toString())
+            }
+
+        var mySessionId = 0
+        val listener = SplitInstallStateUpdatedListener {
+            mySessionId = it.sessionId()
+            when (it.status()) {
+                SplitInstallSessionStatus.DOWNLOADING -> {
+                    val totalBytes = it.totalBytesToDownload()
+                    val progress = it.bytesDownloaded()
+                    // Update progress bar.
+                }
+                SplitInstallSessionStatus.INSTALLING -> Log.d("Status", "INSTALLING")
+                SplitInstallSessionStatus.INSTALLED -> Log.d("Status", "INSTALLED")
+                SplitInstallSessionStatus.FAILED -> Log.d("Status", "FAILED")
+                SplitInstallSessionStatus.REQUIRES_USER_CONFIRMATION -> startIntentSender(
+                    it.resolutionIntent().intentSender,
+                    null,
+                    0,
+                    0,
+                    0
+                )
+
+            }
+
+        }
     }
 }
